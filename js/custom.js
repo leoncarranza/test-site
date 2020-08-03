1d46acf45db4aa47b52eeee2311dabdcb1ac52e0
leoncarranza <leoncarranza@users.noreply.github.com>
2020-08-02 20:30:25 -0400

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+
+(function($) {
+    'use strict';
+
+    // Main Navigation
+    $( '.hamburger-menu' ).on( 'click', function() {
+        $(this).toggleClass('close');
+        $('.site-branding').toggleClass('hide');
+        $('.site-navigation').toggleClass('show');
+        $('.site-header').toggleClass('no-shadow');
+    });
+
+    $(document).ready(function() {
+    $('.image-link').magnificPopup({type:'image'});
+    });
+    
+    // Scroll to Next Section
+    $( '.scroll-down' ).click(function() {
+        $( 'html, body' ).animate({
+            scrollTop: $( '.scroll-down' ).offset().top + 100
+        }, 800 );
+    });
+})(jQuery);
